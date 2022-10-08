98e214c2fc47d978fcb84eec75c2d0a0a40caeed
Hildegunn80 <94168921+Hildegunn80@users.noreply.github.com>
2022-10-08 13:51:05 +0200

    q1 solved

--- question-1.js
+++ question-1.js
@@ -1,13 +1,14 @@
 
-//function getRemainder(a,b) {
-//   return a % b;
-//}
+
+function getRemainderOrg(a,b) {
+   return a % b;
+}
 
 // Convert to an arrow function
 console.log("Oppgave 1:");
 
 const getRemainder = (a,b) => a % b;
 
-console.log("Reminder: " + getRemainder(20,4) );
-
+console.log("Reminder Org: " + getRemainderOrg(13, 5) );
+console.log("Reminder: " + getRemainder(13, 5) );
 
